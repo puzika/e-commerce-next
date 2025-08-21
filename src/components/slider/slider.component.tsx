0d7e14651df58dfee3cf3231b7d97a01a612dea7
puzika <tolibjonlatipovk@gmail.com>
2025-08-22 00:24:16 +0700

    added slider buttons

--- src/components/slider/slider.component.tsx
+++ src/components/slider/slider.component.tsx
@@ -1,69 +1,78 @@
 'use client';
 
+import { useState } from 'react';
 import type { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import Image from 'next/image';
 import MensWatch from "public/men's-watch-slider-img.webp";
 import Laptop from "public/laptop-slider-img.webp";
-import Smarphone from "public/smartphone-slider-img.webp";
+import Smartphone from "public/smartphone-slider-img.webp";
 import Sunglasses from "public/sunglasses-slider-img.webp";
 import Fragrance from "public/fragrance-slider-img.webp";
 import styles from './slider.module.scss';
 
 type SlideProps = {
-  image: string | StaticImport,
-  alt: string,
   pos: number,
+  src: StaticImport | string,
+  alt: string,
   priority?: boolean,
 }
 
-function Slide({ pos, image, alt, priority }: SlideProps) {
+function Slide({ pos, src, alt, priority }: SlideProps) {
   return (
-    <div style={{ translate: `${pos * 100}% 0`}} className={styles.slide}>
+    <div 
+      style={{ translate: `${pos * 100}% 0`}} 
+      className={styles.slide}
+    >
       <Image 
         className={styles.slideImage}
-        src={image}
-        alt={alt}
         fill
+        src={src}
+        alt={alt}
         priority={!!priority}
       />
     </div>
   )
 }
 
-function SlideBtn() {
-  return (
-    <button className={styles.btn}></button>
-  )
-}
-
-type SliderProps = {
+type Slider = {
   priority?: boolean,
 }
 
-export default function Slider({ priority }: SliderProps) {
-  const images: { image: StaticImport, alt: string }[] = [
-    { image: MensWatch, alt: "Men's watches" },
-    { image: Laptop, alt: "Laptop" },
-    { image: Smarphone, alt: "Smartphone" },
-    { image: Sunglasses, alt: "Sunglasses" },
+export default function Slider({ priority }: Slider) {
+  const [currSlide, setCurrSlide] = useState<number>(0);
+
+  const slides: { image: StaticImport, alt: string }[] = [
+    { image: MensWatch, alt: "Men's watches"},
+    { image: Laptop, alt: "Laptops"},
+    { image: Smartphone, alt: "Smartphones"},
+    { image: Sunglasses, alt: "Sunglasses"},
     { image: Fragrance, alt: "Fragrance" },
-  ];
+  ]
 
   return (
     <div className={styles.slider}>
       {
-        images.map(({ image, alt }, idx) => (
+        slides.map(({ image, alt }, idx) => (
           <Slide 
-            key={crypto.randomUUID()} 
-            pos={idx} 
-            image={image} 
-            alt={alt}
-            priority={priority}
+            key={`slider-slide-${idx}`} 
+            pos={idx - currSlide} 
+            src={image} 
+            alt={alt} 
+            priority={priority} 
           />
         ))
       }
-      <div className={styles.buttons}>
-
+      <div className={styles.btns}>
+        {
+          slides.map((_, idx) => (
+            <button
+              key={`slider-btn-${idx}`}
+              className={`${styles.btn} ${idx === currSlide ? styles.btnActive : ''}`}
+              onClick={() => setCurrSlide(idx)}
+            >
+            </button>
+          ))
+        }
       </div>
     </div>
   )

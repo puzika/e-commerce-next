640af21b60a56393ea8b5709675fd2f8679efb99
puzika <tolibjonlatipovk@gmail.com>
2025-08-23 18:11:36 +0700

    added text to slides

--- src/components/slider/slider.component.tsx
+++ src/components/slider/slider.component.tsx
@@ -3,30 +3,36 @@
 import { useState } from 'react';
 import type { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import Image from 'next/image';
-import MensWatch from "public/men's-watch-slider-img.webp";
-import Laptop from "public/laptop-slider-img.webp";
-import Smartphone from "public/smartphone-slider-img.webp";
-import Sunglasses from "public/sunglasses-slider-img.webp";
-import Fragrance from "public/fragrance-slider-img.webp";
+import Link from 'next/link';
+import slides from '@/slides';
 import styles from './slider.module.scss';
 
 type SlideProps = {
   pos: number,
-  src: StaticImport | string,
+  image: StaticImport | string,
   alt: string,
+  title: string,
+  slogan: string,
   priority?: boolean,
 }
 
-function Slide({ pos, src, alt, priority }: SlideProps) {
+function Slide({ pos, image, alt, priority, title, slogan }: SlideProps) {
   return (
     <div 
       style={{ translate: `${pos * 100}% 0`}} 
       className={styles.slide}
     >
+      <div className={styles.slideText}>
+        <p className={styles.slideTitle}>{title}</p>
+        <p className={styles.slideSlogan}>{slogan}</p>
+        <Link className={styles.link} href={'#'}>
+          <span>Shop now</span>
+          <span>&rarr;</span>
+        </Link>
+      </div>
       <Image 
         className={styles.slideImage}
-        fill
-        src={src}
+        src={image}
         alt={alt}
         priority={!!priority}
       />
@@ -41,23 +47,17 @@ type Slider = {
 export default function Slider({ priority }: Slider) {
   const [currSlide, setCurrSlide] = useState<number>(0);
 
-  const slides: { image: StaticImport, alt: string }[] = [
-    { image: MensWatch, alt: "Men's watches"},
-    { image: Laptop, alt: "Laptops"},
-    { image: Smartphone, alt: "Smartphones"},
-    { image: Sunglasses, alt: "Sunglasses"},
-    { image: Fragrance, alt: "Fragrance" },
-  ]
-
   return (
     <div className={styles.slider}>
       {
-        slides.map(({ image, alt }, idx) => (
+        slides.map(({ image, alt, title, slogan }, idx) => (
           <Slide 
             key={`slider-slide-${idx}`} 
             pos={idx - currSlide} 
-            src={image} 
+            image={image} 
             alt={alt} 
+            title={title}
+            slogan={slogan}
             priority={priority} 
           />
         ))
---
+++ src/slides.ts
@@ -0,0 +1,23 @@
+import type { StaticImport } from "next/dist/shared/lib/get-img-props";
+import MensWatch from "public/men's-watch-slider-img.webp";
+import Laptop from "public/laptop-slider-img.webp";
+import Smartphone from "public/smartphone-slider-img.webp";
+import Sunglasses from "public/sunglasses-slider-img.webp";
+import Fragrance from "public/fragrance-slider-img.webp";
+
+type Slide = {
+  image: StaticImport | string,
+  alt: string,
+  title: string,
+  slogan: string,
+}
+
+const slides: Slide[] = [
+  { image: MensWatch, alt: "Men's watches", title: "Men's watches", slogan: "Where tradition meets precision" },
+  { image: Laptop, alt: "Laptops", title: "Laptops", slogan: "Your partner in performance" },
+  { image: Smartphone, alt: "Smartphones", title: "Smartphones", slogan: "Never Miss a Shot" },
+  { image: Sunglasses, alt: "Sunglasses", title: "Sunglasses", slogan: "Live in your shade" },
+  { image: Fragrance, alt: "Fragrances", title: "Fragrances", slogan: "Command the air around you" },
+]
+
+export default slides;

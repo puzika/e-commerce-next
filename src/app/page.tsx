0b1e2bfa353de12b0be1ab3fc3f8c609d397cee3
puzika <tolibjonlatipovk@gmail.com>
2025-08-24 00:05:46 +0700

    added categories section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Sidebar from "@/components/sidebar/sidebar.component"
 import Slider from "@/components/slider/slider.component";
+import Section from "@/components/section/section.component";
 import styles from './page.module.scss';
 
 export default function Home() {
   return (
-    <div className={styles.horizontal}>
-      <Sidebar />
-      <Slider priority />
-    </div>
+    <>
+      <div className={styles.horizontal}>
+        <Sidebar />
+        <Slider priority />
+      </div>
+      <Section title="categories" heading="browse by category" />
+    </>
   )
 }
---
+++ src/components/scroll-btn/scroll-btn.component.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { 
+  GoArrowLeft, 
+  GoArrowRight, 
+  GoArrowUp, 
+  GoArrowDown 
+} from 'react-icons/go';
+import type { ReactNode } from 'react';
+import { IconContext } from 'react-icons';
+import styles from './scroll-btn.module.scss';
+
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+type ScrollBtnProps = {
+  direction: Direction,
+  handler: () => void,
+}
+
+export default function ScrollBtn({ direction, handler }: ScrollBtnProps) {
+  const icons: Record<Direction, ReactNode> = {
+    up: <GoArrowUp />,
+    down: <GoArrowDown />,
+    left: <GoArrowLeft />,
+    right: <GoArrowRight />,
+  }
+
+  return (
+    <button onClick={() => {}} className={styles.btn}>
+      <IconContext value={{ size: "2.5rem" }}>
+        { icons[direction] }
+      </IconContext>
+    </button>
+  )
+}
